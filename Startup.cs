7767101920b1ea45424887f459c0f27df02524a4
Aiymkan Raiapova <evaartray@gmail.com>
2021-02-01 19:59:03 +0100

    Added Country Selector

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using tutorial_components.Data;
+using System.Globalization;
 
 namespace tutorial_components
 {
